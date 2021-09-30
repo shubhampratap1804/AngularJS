d7eecd81149575be41f2b228fdc924ea916d234e
shubhampratap1804 <samsunggalaxygrand2pratap@gmail.com>
2021-10-01 00:02:35 +0530

    Added: username validation using event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ export class AppComponent {
   url;
   //TypeScript
   userName: string = "";
+  nameError: string = "";
 
   ngOnInit(): void {
     this.title = "Hello from Bridgelabz.";
@@ -21,5 +22,15 @@ export class AppComponent {
   onClick($event){
     console.log("Save button is clicked!", $event);
     window.open(this.url, "_blank");
+  }
+
+  onInput($event){
+    console.log("Change Event Occurred!", $event.data);
+    const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    if (nameRegex.test(this.userName)) {
+      this.nameError = "";
+      return;
+    }
+    this.nameError = "Name is Incorrect!";
   }
 }
